488c34e7de26b9b866c0edc72b0d6194fbfe0134
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-08-13 01:40:57 +0600

    Wallet Analytics page, Investment details and Pricing page added

--- src/pages/UserDashboard/Analytics.jsx
+++ src/pages/UserDashboard/Analytics.jsx
@@ -1,69 +1,16 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from "@/components/ui/menubar";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoNotifications } from "react-icons/io5";
+import Wallet from "../wallet/Wallet";
+import Header from "./Header";
 
 const Analytics = () => {
-    return (
-        <>
-        {/* Header Section------------- */}
-        <section>
-            {/* --------Header Avater */}
-            <div className="flex items-center gap-2 justify-end p-4 lg:p-5">
-                    <div className="flex items-center gap-28 md:gap-5 sm:flex-row-reverse">
-                            <div className="flex gap-3">
-                                <div className="flex sm:flex-row-reverse gap-3 items-center">
-                                    <Avatar>
-                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                    </Avatar>
-                                    <div>
-                                        <p className="font-bold text-xl">Shakil</p>
-                                        <p className="text-[#8F8F8F] font-semibold">User Id: TODO</p>
-                                    </div>
-                                </div>    
-                            </div>    
-                            <div className="">
-                                <IoNotifications></IoNotifications>
-                            </div>
-                    </div>
-                    
-                    <div className="hidden sm:block">
-                            <Menubar>
-                                <MenubarMenu>
-                                    <MenubarTrigger><IoIosArrowDown></IoIosArrowDown></MenubarTrigger>
-                                    <MenubarContent className='mt-5'>
-                                    <MenubarItem>
-                                        Settings <MenubarShortcut>⌘T</MenubarShortcut>
-                                    </MenubarItem>
-                                    <MenubarSeparator/>
-                                    <MenubarItem>Print</MenubarItem>
-                                    <MenubarSeparator />
-                                    <MenubarItem>Share</MenubarItem>
-                                    <MenubarSeparator />
-                                    <MenubarItem>Logout</MenubarItem>
-                                    </MenubarContent>
-                                </MenubarMenu>
-                            </Menubar>
-                    </div>
-                </div>
-        </section>
-        {/* Main Content Section-------- */}
-        <section className="p-4 grid sm:grid-cols-12 gap-4">
-        <div className="bg-red-500 min-h-[100px]"></div>
-                <div className="bg-yellow-500 min-h-[100px]"></div>
-                <div className="bg-blue-500 min-h-[100px]"></div>
-                <div className="bg-orange-500 min-h-[100px]"></div>
-                <div className="bg-teal-500 min-h-[100px]"></div>
-                <div className="bg-amber-700 min-h-[100px]"></div>
-                <div className="bg-gray-400 min-h-[100px]"></div>
-                <div className="bg-purple-500 min-h-[100px]"></div>
-                <div className="bg-sky-500 min-h-[100px]"></div>
-                
-        </section>
-        </>
-        
-    );
+  return (
+    <>
+      {/* Header Section------------- */}
+      <Header />
+      {/* -------------Wallet Section------------- */}
+
+      <Wallet />
+    </>
+  );
 };
 
 export default Analytics;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,57 +3,66 @@ import Analytics from "@/pages/UserDashboard/Analytics";
 import Balance from "@/pages/UserDashboard/Balance";
 import Home from "@/pages/UserDashboard/Home";
 import Orders from "@/pages/UserDashboard/Orders";
+import Pricing from "@/pages/UserDashboard/Pricing";
 import ResellerReferrals from "@/pages/UserDashboard/ResellerReferrals";
 import Settings from "@/pages/UserDashboard/Settings";
 import Store from "@/pages/UserDashboard/Store";
 import UserDashboard from "@/pages/UserDashboard/UserDashboard";
+import InvestmentListPage from "@/pages/wallet/InvestmentListPage";
 
-import {  createBrowserRouter } from "react-router-dom";
-
+import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App></App>
-    },
-    {   
-        path: '/user-dashboard',
-        element: <UserDashboard></UserDashboard>,
-        children: [
-                {
-                    path: '/user-dashboard/home',
-                    element: <Home></Home>      
-                },
-           
-            // {
-               
-            //     path: 'home',
-            //     element: <Home></Home>
-            // },
-            {
-                path: 'store',
-                element: <Store></Store>
-            },
-            {
-                path: 'orders',
-                element: <Orders></Orders>
-            },
-            {
-                path: 'reseller-referrals',
-                element: <ResellerReferrals></ResellerReferrals>
-            },
-            {
-                path: 'balance',
-                element: <Balance></Balance>
-            },
-            {
-                path: 'analytics',
-                element: <Analytics></Analytics>
-            },
-            {
-                path: 'settings',
-                element: <Settings></Settings>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/user-dashboard",
+    element: <UserDashboard></UserDashboard>,
+    children: [
+      {
+        path: "/user-dashboard/home",
+        element: <Home></Home>,
+      },
+
+      // {
+
+      //     path: 'home',
+      //     element: <Home></Home>
+      // },
+      {
+        path: "store",
+        element: <Store></Store>,
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>,
+      },
+      {
+        path: "reseller-referrals",
+        element: <ResellerReferrals></ResellerReferrals>,
+      },
+      {
+        path: "balance",
+        element: <Balance></Balance>,
+      },
+      {
+        path: "analytics",
+        element: <Analytics></Analytics>,
+      },
+      {
+        path: "settings",
+        element: <Settings></Settings>,
+      },
+      {
+        path: "pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "/user-dashboard/investment_list",
+        element: <InvestmentListPage />,
+      },
+    ],
+  },
+]);
