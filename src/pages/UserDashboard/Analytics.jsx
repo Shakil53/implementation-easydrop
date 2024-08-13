7b8fa79ba2d9c6c23182eb06a680ee6bcc94ab08
Shakil53 <shakilahmed.dcc@gmail.com>
2024-08-13 13:09:23 +0600

    analytics page - two content done

--- src/pages/UserDashboard/Analytics.jsx
+++ src/pages/UserDashboard/Analytics.jsx
@@ -1,13 +1,34 @@
+import { data } from "@/api/analyticsData";
+import { COLORS, dataForRevenue, renderCustomizedLabel, style } from "@/api/revenueData";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from "@/components/ui/menubar";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoNotifications } from "react-icons/io5";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { Area, AreaChart, CartesianGrid, Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+
+
 
 const Analytics = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(null);
+    
+    const optionsYear = ['2024', '2023', '2022', '2021', '2020'].map(year => ({ value: year, label: year }));
+    
+    const toggleDropdown = () => setIsOpen(!isOpen);
+    const handleSelect = (option) => {
+      setSelectedOption(option);
+      setIsOpen(false);
+    };
+  
+
+
     return (
         <>
         {/* Header Section------------- */}
-        <section>
             {/* --------Header Avater */}
             <div className="flex items-center gap-2 justify-end p-4 lg:p-5">
                     <div className="flex items-center gap-28 md:gap-5 sm:flex-row-reverse">
@@ -47,20 +68,111 @@ const Analytics = () => {
                             </Menubar>
                     </div>
                 </div>
-        </section>
+       
         {/* Main Content Section-------- */}
-        <section className="p-4 grid sm:grid-cols-12 gap-4">
-        <div className="bg-red-500 min-h-[100px]"></div>
-                <div className="bg-yellow-500 min-h-[100px]"></div>
-                <div className="bg-blue-500 min-h-[100px]"></div>
-                <div className="bg-orange-500 min-h-[100px]"></div>
+            <div className="p-4 grid sm:grid-cols-12 gap-4">
+               {/* Content------------1 */}
+                <div className="sm:col-span-4 rounded-lg border p-4 shadow-md">
+                    {/* heading--- */}
+                    <div className="flex justify-between">
+                        <h1 className="font-medium text-2xl">Market Overview</h1>
+                        <div className="relative inline-block w-16">
+                            <div className="bg-white border border-gray-300 rounded-md shadow-sm p-2 flex justify-between items-center cursor-pointer"
+                                onClick={toggleDropdown}
+                            ><span>{selectedOption ? selectedOption.label : 'Year'}</span>
+                            <MdOutlineKeyboardArrowDown className={` transform transition-transform ${isOpen ? 'rotate-180' : ''}`}></MdOutlineKeyboardArrowDown>
+                            </div>
+                            {isOpen && (
+                                <ul className="absolute mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg z-10">
+                                {optionsYear.map((option) => (
+                                    <li
+                                    key={option.value}
+                                    className="p-2 hover:bg-gray-100 cursor-pointer"
+                                    onClick={() => handleSelect(option)}
+                                    >{option.label}
+                                    </li>
+                                ))}
+                                </ul>
+                            )}
+                        </div>
+
+                    </div>
+                        {/* Graph chart ---------------------- */}
+                     <ResponsiveContainer width="100%" height="85%">
+                            <AreaChart width={730} height={250} data={data}
+                                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                                <defs>
+                                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#FF0080" stopOpacity={0.8}/>
+                                    <stop offset="95%" stopColor="#522F8F" stopOpacity={0}/>
+                                    </linearGradient>
+                                    
+                                </defs>
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="uv" stroke="#FF0080" fillOpacity={1} fill="url(#colorUv)" />
+                            
+                                </AreaChart>
+                                        {/* <Area type="monotone" dataKey="uv" stroke="#FF0080" fill="#522F8F"/> */}
+                        </ResponsiveContainer>
+                    
+                </div>
+                {/* Content------------2 */}
+                <div className="sm:col-span-4 rounded-lg border p-4 shadow-md">
+                     {/* heading--- */}
+                     <div className="flex justify-between">
+                        <h1 className="font-medium text-2xl">Revenue</h1>
+                        <Select>
+                            <SelectTrigger className="w-[100px] sm:w-[150px]">
+                                <SelectValue placeholder="This month" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                <SelectLabel>Months</SelectLabel>
+                                <SelectItem value="apple">Jan - April</SelectItem>
+                                <SelectItem value="pineapple">May- Aug</SelectItem>
+                                <SelectItem value="mango">Sep - Dec</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                     </div>
+
+                     <div style={{ width: '100%', height: 250 }}>
+                        <ResponsiveContainer>
+                            <PieChart>
+                                <Pie
+                                    data={dataForRevenue}
+                                    cx="50%"
+                                    cy="50%"
+                                    labelLine={false}
+                                    label={renderCustomizedLabel}
+                                    outerRadius={80}
+                                    fill="#8884d8"
+                                    dataKey="value"
+                                >
+                                    {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                    ))}
+                                </Pie>
+                                <Legend iconSize={10} layout="horizontal" horizOriginX="bottom" wrapperStyle={style} />
+                            </PieChart>
+                        </ResponsiveContainer>
+                        
+                    </div>
+
+                    
+                </div>
+                <div className="bg-blue-500 sm:col-span-4 min-h-[185px]"></div>
+                <div className="bg-orange-500 sm:col-span-4 min-h-24"></div>
                 <div className="bg-teal-500 min-h-[100px]"></div>
                 <div className="bg-amber-700 min-h-[100px]"></div>
                 <div className="bg-gray-400 min-h-[100px]"></div>
                 <div className="bg-purple-500 min-h-[100px]"></div>
                 <div className="bg-sky-500 min-h-[100px]"></div>
                 
-        </section>
+        </div>
         </>
         
     );
--- src/pages/UserDashboard/Balance.jsx
+++ src/pages/UserDashboard/Balance.jsx
@@ -74,7 +74,7 @@ const Balance = () => {
                                         <h1 className="text-xl font-semibold">Current Balance</h1>
                                         <Button className="bg-[#6366F1] rounded-2xl px-3 py-2 text-white">Request Payment</Button>
                                     </div>
-                                    <h1 className="text-4xl font-bold">TK. 200</h1>
+                                    <h1 className="text-3xl font-bold">TK. 200</h1>
                                     <div className="flex items-center gap-2">
                                         <div className="bg-[#00E676] bg-opacity-20 size-5 rounded-full flex items-center justify-center">
                                         <ArrowUp className=" text-[#00E676] size-4"></ArrowUp> 
@@ -91,7 +91,7 @@ const Balance = () => {
                                                     <h1 className="text-xl font-semibold">Request Payment</h1>
                                                     <TbCoinTaka className="size-8 text-[#6366F1]"></TbCoinTaka>
                                                 </div>
-                                                <h1 className="text-4xl font-bold">TK. 500</h1>
+                                                <h1 className="text-3xl font-bold">TK. 500</h1>
                                                 <div className="flex items-center gap-2">          
                                                     <div className="bg-[#00E676] bg-opacity-20 size-5 rounded-full flex items-center justify-center">
                                                     <ArrowUp className=" text-[#00E676] size-4"></ArrowUp> 
@@ -107,9 +107,9 @@ const Balance = () => {
                     <div className="border rounded-lg p-3 col-span-4 shadow-md">
                                 <div className="flex justify-between items-center">
                                         <h1 className="text-xl font-semibold">Commission Earned</h1>
-                                    </div>
+                                </div>
                                 <div className="flex justify-between">
-                                    <h1 className="text-4xl font-bold">TK. 300</h1>
+                                    <h1 className="text-3xl font-bold">TK. 300</h1>
                                     <Select>
                                         <SelectTrigger className="w-[100px] sm:w-[150px]">
                                             <SelectValue placeholder="Last 7 payment" />
@@ -144,22 +144,47 @@ const Balance = () => {
                                     </BarChart>
                                 </ResponsiveContainer>
                     </div>
-                    {/* Card Section 4 ----------Graph chart 2*/}
+                    {/* Card section 4--------------------- */}
                     <div className="border rounded-lg p-3 col-span-4 shadow-md">
-                    
-                            <ResponsiveContainer width="100%" height="100%">
-                                    <BarChart width={150} height={40} data={data}>
-                                        <XAxis dataKey="name" scale="point"  padding={{ left: 18, right: 20}} />
-                                        <YAxis dataKey='uv'/>
-                                        <Tooltip dataKey='pv' />
-                                        <Legend />
-                                        <CartesianGrid strokeDasharray="3 3" />
-                                        <Bar dataKey="uv"  barSize={20} fill="#F163DA" background={{ fill: '#eee' }} />
-                                    </BarChart>
-                                </ResponsiveContainer>
-                                
+                                <div className="flex justify-between items-center">
+                                        <h1 className="text-xl font-semibold">Recived Payment</h1>
+                                </div>
+                                <div className="flex justify-between">
+                                    <h1 className="text-3xl font-bold">TK. 200</h1>
+                                    <Select>
+                                        <SelectTrigger className="w-[100px] sm:w-[150px]">
+                                            <SelectValue placeholder="Last 7 payment" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                            <SelectLabel>Months</SelectLabel>
+                                            <SelectItem value="apple">Jan - April</SelectItem>
+                                            <SelectItem value="pineapple">May- Aug</SelectItem>
+                                            <SelectItem value="mango">Sep - Dec</SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <div className="bg-[#00E676] bg-opacity-20 size-5 rounded-full flex items-center justify-center">
+                                    <ArrowUp className=" text-[#00E676] size-4"></ArrowUp> 
+                                    </div> 
+                                    <div>
+                                        <p><span className="text-[#00E676] text-sm">+50%</span> <span className="text-[#949494]">From last week</span></p>
+                                    </div>
+                                </div>
+                        {/* Graph chart ---------------------- */}
+                        <ResponsiveContainer width="100%" height="70%">
+                            <BarChart width={150} height={40} data={data}>
+                                <XAxis dataKey="name" scale="point"  padding={{ left: 18, right: 20}} />
+                                <YAxis dataKey='uv'/>
+                                <Tooltip dataKey='pv' />
+                                <Legend />
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <Bar dataKey="uv"  barSize={20} fill="#F163DA" background={{ fill: '#eee' }} />
+                            </BarChart>
+                    </ResponsiveContainer>
                     </div>
-                    
                 </div>
                 {/* Payment Request table----------------- */}
                 <div className="border mt-4 p-2 rounded-lg shadow-md">
